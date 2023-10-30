e93f8cc984c5fe556bf84548ed5efe53d22bff02
Pemula Cupu <120018241+akupemulacupu@users.noreply.github.com>
2023-10-31 00:23:10 +0700

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // set the date we're counting down to
-var target_date = new Date('July,10,2021').getTime();
+var target_date = new Date('July,10,2024').getTime();
 
 // variables for time units
 var days, hours, minutes, seconds;
